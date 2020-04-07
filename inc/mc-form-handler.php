80da32ea24afe34ce8ae3e080faa029fbd70d4b1
Milan Peno <penomilan@gmail.com>
2020-04-07 16:50:27 +0200

    subscribe form test

---
+++ inc/mc-form-handler.php
@@ -0,0 +1,48 @@
+﻿<?php 
+$errors = '';
+$myemail = 'penomilan@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['email']))
+{
+    $errors .= "\n ERROR";
+}
+ 
+$email_address = $_POST['email']; 
+ 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n ERROR";
+}
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Subscribe form submission:";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Email: $email_address "; 
+	
+	$headers = "From: $myemail\n"; 
+	$headers .= "Reply-To: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: ../thanks.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+ 
+
+</body>
+</html>
